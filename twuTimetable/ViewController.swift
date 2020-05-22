2f0241aebcf28d96ea5ee9f31d013e78001a10f6
Jack Park <jackp7640@gmail.com>
2020-05-21 21:29:20 -0700

    Initial Commit

---
+++ twuTimetable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twuTimetable
+//
+//  Created by Jack Park on 2020-05-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
